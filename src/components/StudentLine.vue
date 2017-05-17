cb27ba096466611acfc76697ecefc92907fcb154
Rabin Julien <julienr114@gmail.com>
2017-05-17 16:30:18 +0200

    initial commit

---
+++ src/components/StudentLine.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    props: ['student'],
+    methods: {
+      remove() {
+        this.$emit('remove');
+      },
+    },
+  };
+</script>
+
+<template lang="pug">
+  div
+    | {{student.firstname}}
+    button(@click="remove") Supprimer
+</template>
+
+<style>
+</style>
---
+++ src/components/StudentTable.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+import StudentLine from './StudentLine';
+
+export default {
+  components: {
+    StudentLine,
+  },
+  data() {
+    return {
+      students: [],
+    };
+  },
+  methods: {
+    getAll() {
+      axios.get('http://localhost:3000/students')
+      .then((response) => {
+        this.students = response.data;
+      });
+    },
+    remove(index) {
+      this.students.splice(index, 1);
+    },
+  },
+  mounted() {
+    this.getAll();
+  },
+};
+</script>
+
+<template>
+  <div>
+    <student-line v-for='(student, index) in students' :student="student" v-on:remove="remove(index)"> </student-line>
+  </div>
+</template>
+
+<style>
+</style>
