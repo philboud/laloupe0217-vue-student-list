0542a2b47f236c9ac486c56f1c9c1756bab3c96c
Philippe <philippe.boudet42@gmail.com>
2017-05-23 10:10:45 +0200

    studentlist

--- src/components/StudentTable.vue
+++ src/components/StudentTable.vue
@@ -1,9 +1,11 @@
 <script>
 import axios from 'axios';
 import StudentLine from './StudentLine';
+import addStudent from './addStudent';
 
 export default {
   components: {
+    addStudent,
     StudentLine,
   },
   data() {
@@ -14,12 +16,22 @@ export default {
   methods: {
     getAll() {
       axios.get('http://localhost:3000/students')
-      .then((response) => {
-        this.students = response.data;
-      });
+        .then((response) => {
+          this.students = response.data;
+        });
+    },
+    addOne(a) {
+      const newStudent = { firstname: a };
+      axios.post('http://localhost:3000/students', newStudent)
+        .then((res) => {
+          this.students.push(res.data);
+        });
     },
     remove(index) {
-      this.students.splice(index, 1);
+      axios.delete(`http://localhost:3000/students/${this.students[index].id}`)
+        .then(() => {
+          this.students.splice(index, 1);
+        });
     },
   },
   mounted() {
@@ -29,10 +41,12 @@ export default {
 </script>
 
 <template>
-  <div>
-    <student-line v-for='(student, index) in students' :student="student" v-on:remove="remove(index)"> </student-line>
-  </div>
+<div>
+  <student-line v-for='(student, index) in students' :student="student" v-on:remove="remove(index)"> </student-line>
+  <add-student v-on:addOne="addOne"></add-student>
+</div>
 </template>
 
 <style>
+
 </style>
---
+++ src/components/addStudent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+  data() {
+    return {
+      firstname: '',
+    };
+  },
+  methods: {
+    addOne() {
+      this.$emit('addOne', this.firstname);
+    },
+  },
+};
+</script>
+
+<template>
+<div>
+  <input type="text" v-model="firstname" placeholder="prenom">
+  <button v-on:click="addOne">Ajouter</button>
+</div>
+</template>
+
+<style>
+
+</style>
